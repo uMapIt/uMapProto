49ff23a5f3fe92d53a971a0db10fbcc9c6e42cee
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-31 02:00:28 +0900

    add links to list view to detail view page

--- client/components/listings/list-view-tile.js
+++ client/components/listings/list-view-tile.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class ListViewTile extends Component {
   render() {
     return (
       <div className="col-md-4">
-        <h3>{this.props.listing.title}</h3>
+        <h3><Link to={`/listings/${this.props.listing._id}`}>{this.props.listing.title}</Link></h3>
       </div>
     );
   }
