47b5b4236122d3d8d27c23ae86abf1d141df2203
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-15 22:57:54 +0900

    replace map engine

--- client/templates/map.js
+++ client/templates/map.js
@@ -1,65 +1,66 @@
 import { Markers } from '../../imports/collections/markers';
-import { Listings } from '../../imports/collections/listings'
-import { ReactiveVar } from 'meteor/reactive-var';
-import { Link, browserHistory } from 'react-router';
 
-if (Meteor.isClient) {
-  Session.setDefault('centerLat', 35.157354);
-  Session.setDefault('centerLng', 129.059168);
-  Session.setDefault('zoom', 13);
-  Session.setDefault('makerId', '');
-
-  GoogleMaps.setConfig('helpers.getInfoWindowContent', function(item) {
-    return item.name || 'item-' + item._id;
+Meteor.startup(function() {
+  GoogleMaps.load({
+    key: 'AIzaSyAzR7xL05N-ecp76GFheDmXqNnqMiDdmnI'
   });
+});
 
-  GoogleMaps.setConfig('helpers.isInfoWindowOpen', function(item) {
-    return Session.get('infoWindowShow-' + item._id);
-  });
-
-  Template.map.helpers({
-    centerLat: function() {
-      return Session.get('centerLat');
-    },
-
-    centerLng: function() {
-      return Session.get('centerLng');
-    },
-
-    zoom: function() {
-      return 10;
-    },
-
-    items: function() {
-      return Template.instance().markers();
+Template.map.helpers({
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(35.159117, 129.045707),
+        zoom: 11
+      };
     }
-  });
+  }
+});
 
-  Template.map.onCreated(function() {
-    let instance = this;
-
-    instance.autorun(function() {
-      let subscription = instance.subscribe('markers.all');
+Template.map.onCreated(function() {
+  GoogleMaps.ready('map', function(map) {
+    google.maps.event.addListener(map.instance, 'click', function(event) {
+      Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
     });
 
-    instance.markers = function() {
-      return Markers.find({});
-    }
+    var markers = {};
+
+    Markers.find().observe({
+      added: function(document) {
+        // Create a marker for this document
+        var marker = new google.maps.Marker({
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: new google.maps.LatLng(document.lat, document.lng),
+          map: map.instance,
+          // We store the document _id on the marker in order
+          // to update the document within the 'dragend' event below.
+          id: document._id
+        });
+
+        // This listener lets us drag markers on the map and update their corresponding document.
+        google.maps.event.addListener(marker, 'dragend', function(event) {
+          Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+        });
+
+        // Store this marker instance within the markers object.
+        markers[document._id] = marker;
+      },
+      changed: function(newDocument, oldDocument) {
+        markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+      },
+      removed: function(oldDocument) {
+        // Remove the marker from the map
+        markers[oldDocument._id].setMap(null);
+
+        // Clear the event listener
+        google.maps.event.clearInstanceListeners(
+          markers[oldDocument._id]);
+
+        // Remove the reference to this marker instance
+        delete markers[oldDocument._id];
+      }
+    });
+
   });
-
-  Template.map.events({
-
-    'map_click .map': function(event) {
-      let lat = event.originalEvent.detail.lat;
-      let lng = event.originalEvent.detail.lng;
-      let icon = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + Math.floor(Math.random() * 10) + '|FF0000|FFFFFF';
-
-      Meteor.call('markers.create', lat, lng, icon, (err, markerId) => {
-        Session.set('markerId', markerId);
-      });
-    }
-    
-  });
-
-
-}
+});
