7ca4bff613c8fcc747a6823d362cd1c3fefbce8f
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-17 01:42:01 +0900

    restrict /create route to logged in users.

--- client/main.js
+++ client/main.js
@@ -10,11 +10,20 @@ import ListingsDetail from './components/listings/ListingsDetail';
 import ListingsView from './components/listings/ListingsView';
 import ListingsMap from './components/listings/ListingsMap';
 
+const requireAuth = (nextState, replace) => {
+  if (!Meteor.user()) {
+    replace({
+      pathname: '/',
+      state : { nextPathname: nextState.location.pathname }
+    })
+  }
+};
+
 const routes = (
   <Router history={browserHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={ListingsView}/>
-      <Route path='create' component={ListingsCreate} />
+      <Route path='create' component={ListingsCreate} onEnter={requireAuth} />
       <Route path='listings/:listingId' component={ListingsDetail} />
       <Route path='map' component={ListingsMap} />
     </Route>
