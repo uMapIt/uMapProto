1cf7894ad33d684f6955c7ef10ee012bf958bfae
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-18 02:48:49 +0900

    remove unnecessary meteor method

--- imports/collections/markers.js
+++ imports/collections/markers.js
@@ -1,15 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import { check, Match } from 'meteor/check';
 
-Meteor.methods({
-  'markers.create': function(lat, lng, icon) {
-    return Markers.insert({
-      lat: lat,
-      lng: lng,
-      draggable: true,
-      icon: icon
-    });
-  }
-});
+// TODO: add meteor methods here
 
 export const Markers = new Mongo.Collection('markers');
