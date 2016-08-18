65f8670bab9c65fe87c7245a9c995a6385a3e3e0
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-19 03:31:33 +0900

    fix map bug

--- client/templates/map.js
+++ client/templates/map.js
@@ -14,59 +14,54 @@ Template.map.helpers({
 
 Template.map.onCreated(function() {
 
-  let update_timeout = null;
+  var updateTimeout = null;
+  var markerAdded = false;
 
   GoogleMaps.ready('map', function(map) {
 
-    google.maps.event.addListenerOnce(map.instance, 'click', function(event) {
-      update_timeout = setTimeout(function() {
-        Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+    google.maps.event.addListener(map.instance, 'click', function(event) {
+      updateTimeout = setTimeout(function() {
+        if (!markerAdded) {
+          Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() }, function(err, markerId) {
+
+            var markers = {};
+
+            Markers.find({ _id: markerId }).observe({
+
+              added: function(document) {
+                // Create a marker for this document
+                var marker = new google.maps.Marker({
+                  draggable: true,
+                  animation: google.maps.Animation.DROP,
+                  position: new google.maps.LatLng(document.lat, document.lng),
+                  map: map.instance,
+                  // We store the document _id on the marker in order
+                  // to update the document within the 'dragend' event below.
+                  id: document._id
+                });
+
+                // This listener lets us drag markers on the map and update their corresponding document.
+                google.maps.event.addListener(marker, 'dragend', function(event) {
+                  Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+                });
+                // Store this marker instance within the markers object.
+                markers[document._id] = marker;
+              },
+
+              changed: function(newDocument, oldDocument) {
+                markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+              }
+            });
+
+          });
+          markerAdded = true;
+        }
       }, 200);
     });
 
     // Prevents 'click' event from firing off first
     google.maps.event.addListener(map.instance, 'dblclick', function(event) {
-      clearTimeout(update_timeout);
+      clearTimeout(updateTimeout);
     });
-
-    var markers = {};
-
-    Markers.find().observe({
-      added: function(document) {
-        // Create a marker for this document
-        var marker = new google.maps.Marker({
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          position: new google.maps.LatLng(document.lat, document.lng),
-          map: map.instance,
-          // We store the document _id on the marker in order
-          // to update the document within the 'dragend' event below.
-          id: document._id
-        });
-
-        // This listener lets us drag markers on the map and update their corresponding document.
-        google.maps.event.addListener(marker, 'dragend', function(event) {
-          Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-        });
-
-        // Store this marker instance within the markers object.
-        markers[document._id] = marker;
-      },
-      changed: function(newDocument, oldDocument) {
-        markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-      },
-      removed: function(oldDocument) {
-        // Remove the marker from the map
-        markers[oldDocument._id].setMap(null);
-
-        // Clear the event listener
-        google.maps.event.clearInstanceListeners(
-          markers[oldDocument._id]);
-
-        // Remove the reference to this marker instance
-        delete markers[oldDocument._id];
-      }
-    });
-
   });
 });
