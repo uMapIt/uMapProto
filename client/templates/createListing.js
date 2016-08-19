3ec0c4149f394544cc284571b885364b4ec3fb10
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-19 17:34:39 +0900

    remove old comments

--- client/templates/createListing.js
+++ client/templates/createListing.js
@@ -30,22 +30,17 @@ Template.createListing.onCreated(function() {
             Markers.find({ _id: markerId }).observe({
 
               added: function(document) {
-                // Create a marker for this document
                 var marker = new google.maps.Marker({
                   draggable: true,
                   animation: google.maps.Animation.DROP,
                   position: new google.maps.LatLng(document.lat, document.lng),
                   map: map.instance,
-                  // We store the document _id on the marker in order
-                  // to update the document within the 'dragend' event below.
                   id: document._id
                 });
 
-                // This listener lets us drag markers on the map and update their corresponding document.
                 google.maps.event.addListener(marker, 'dragend', function(event) {
                   Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
                 });
-                // Store this marker instance within the markers object.
                 markers[document._id] = marker;
               },
 
