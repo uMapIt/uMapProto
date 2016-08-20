d15bc7ec183a7e7a0c516be1e6fca3d91c5e6afd
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-20 13:58:51 +0900

    update onCreateUser method

--- server/main.js
+++ server/main.js
@@ -13,7 +13,7 @@ Meteor.startup(() => {
 
   Accounts.onCreateUser(function(options, user) {
     if (options.profile) {
-        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?width=200&height=200";
         user.profile = options.profile;
     }
     return user;
